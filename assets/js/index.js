c37833a0549f3763e72118a1c9e54f9cbff8b8f1
Kyle Cho <kylescho99@gmail.com>
2021-02-23 11:11:18 -0500

    config changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,18 @@
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.body.classList.add("dark");
 }
+
+$(document).ready(function () {
+    $(".popup").hide();
+    $(".openpop").click(function (e) {
+        e.preventDefault();
+        $("iframe").attr("src", $(this).attr('href'));
+        $(".links").fadeOut('slow');
+        $(".popup").fadeIn('slow');
+    });
+
+    $(".close").click(function () {
+        $(this).parent().fadeOut("slow");
+        $(".links").fadeIn("slow");
+    });
+});
